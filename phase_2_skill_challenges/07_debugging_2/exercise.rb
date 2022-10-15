2b75d79383d5a7cce32dd09a59cb002f6773abaf
Joseph Lander <josephclander@gmail.com>
2022-10-15 17:56:49 +0100

    Add 07 exercise setup

---
+++ phase_2_skill_challenges/07_debugging_2/exercise.rb
@@ -0,0 +1,20 @@
+# File: exercise.rb
+def factorial(n)
+  product = 1
+  while n > 0
+    # binding.irb # Mystery new line!
+    n -= 1
+    product *= n
+  end
+  product
+end
+
+p factorial(5)
+
+# cd into folder and run: ruby exercise.rb
+
+# you will now get a mini irb terminal in vs code
+
+# type a value and enter to access its current value
+
+# you can put `binding.irb` in multiple places
