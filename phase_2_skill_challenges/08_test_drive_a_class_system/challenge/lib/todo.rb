88ec7c1a3f3b72e073d878bc4617a5f69c9cc605
Joseph Lander <josephclander@gmail.com>
2022-10-17 14:48:31 +0100

    Add 08 challenge Setup

---
+++ phase_2_skill_challenges/08_test_drive_a_class_system/challenge/lib/todo.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: false
+
+# single todo
+class Todo
+  # task is a string
+  def initialize(task)
+    # ...
+  end
+
+  def task
+    # Returns the task as a string
+  end
+
+  def mark_done!
+    # Marks the todo as done
+    # Returns nothing
+  end
+
+  def done?
+    # Returns true if the task is done
+    # Otherwise, false
+  end
+end
---
+++ phase_2_skill_challenges/08_test_drive_a_class_system/challenge/lib/todo_list.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: false
+
+# todolist
+class TodoList
+  def initialize; end
+
+  # TODO: is an instance of Todo
+  def add(todo) 
+    # Returns nothing
+  end
+
+  def incomplete
+    # Returns all non-done todos
+  end
+
+  def complete
+    # Returns all complete todos
+  end
+
+  def give_up!
+    # Marks all todos as complete
+  end
+end
