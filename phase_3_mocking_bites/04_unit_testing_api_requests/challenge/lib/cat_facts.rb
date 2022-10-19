0062bc135be5401b23d33e3d5cdea31717c01fa3
Joseph Lander <josephclander@gmail.com>
2022-10-19 17:34:26 +0100

    Add 04 api testing sol

--- phase_3_mocking_bites/04_unit_testing_api_requests/challenge/lib/cat_facts.rb
+++ phase_3_mocking_bites/04_unit_testing_api_requests/challenge/lib/cat_facts.rb
@@ -5,6 +5,10 @@ require 'net/http'
 
 # cat facts
 class CatFacts
+  def initialize(requester)
+    @requester = requester
+  end
+
   def provide
     "Cat fact: #{get_cat_fact['fact']}"
   end
@@ -12,7 +16,7 @@ class CatFacts
   private
 
   def get_cat_fact
-    text_response = Net::HTTP.get(URI('https://catfact.ninja/fact'))
+    text_response = @requester.get(URI('https://catfact.ninja/fact'))
     JSON.parse(text_response)
   end
 end
--- phase_3_mocking_bites/04_unit_testing_api_requests/challenge/spec/cat_facts_spec.rb
+++ phase_3_mocking_bites/04_unit_testing_api_requests/challenge/spec/cat_facts_spec.rb
@@ -1,2 +1,15 @@
 # frozen_string_literal: false
 
+require 'cat_facts'
+
+describe CatFacts do
+  it 'returns a catfact' do
+    fake_requester = double(:requester)
+    allow(fake_requester).to receive(:get).with(
+      URI('https://catfact.ninja/fact')
+    ).and_return('{"fact":"Many cats love having their forehead gently stroked.","length":52}')
+    cat_fact = CatFacts.new(fake_requester)
+
+    expect(cat_fact.provide).to eq 'Cat fact: Many cats love having their forehead gently stroked.'
+  end
+end
