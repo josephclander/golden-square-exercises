9f82a187b60f00d5789332486ae49cd7bc03c130
Joseph Lander <josephclander@gmail.com>
2022-10-20 10:36:37 +0100

    Add 05 io exercise sol

--- phase_3_mocking_bites/05_unit_testing_terminal_IO/exercise/spec/calculator_spec.rb
+++ phase_3_mocking_bites/05_unit_testing_terminal_IO/exercise/spec/calculator_spec.rb
@@ -1,10 +1,38 @@
-interactive_calculator = InteractiveCalculator.new(Kernel)
-interactive_calculator.run
+# frozen_string_literal: false
 
-# Hello. I will subtract two numbers.
-# Please enter a number
-# 4
-# Please enter another number
-# 3
-# Here is your result:
-# 4 - 3 = 1
+# interactive calculator class
+class InteractiveCalculator
+  def initialize(io)
+    @io = io
+  end
+
+  def run
+    @io.puts 'Hello. I will subtract two numbers.'
+    @io.puts 'Please enter a number'
+    value1 = @io.gets
+    @io.puts 'Please enter another number'
+    value2 = @io.gets
+    @io.puts 'Here is your result:'
+    result = "#{value1} - #{value2} = #{value1 - value2}"
+    @io.puts result
+  end
+end
+
+describe InteractiveCalculator do
+  it 'will subtract two given numbers' do
+    io = double :io
+    expect(io).to receive(:puts).with('Hello. I will subtract two numbers.')
+    expect(io).to receive(:puts).with('Please enter a number')
+    expect(io).to receive(:gets).and_return(4)
+    expect(io).to receive(:puts).with('Please enter another number')
+    expect(io).to receive(:gets).and_return(3)
+    expect(io).to receive(:puts).with('Here is your result:')
+    expect(io).to receive(:puts).with('4 - 3 = 1')
+
+    interactive_calculator = InteractiveCalculator.new(io)
+    interactive_calculator.run
+  end
+end
+
+# interactive_calculator = InteractiveCalculator.new(Kernel)
+# interactive_calculator.run
